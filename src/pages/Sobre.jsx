17dfb78a369ce1f7df14fc2cfc5bb9c500a4f56f
Laiza <laiza.senna26@gmail.com>
2022-10-16 17:23:15 -0300

    add page about

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 import Me from '../assets/Larissa.jpg';
+
+import '../components/StylePages/sobre.css';
 
 function Sobre() {
   return (
     <div className='conteiner-sobre'>
-      {/* <img src={Me} alt='Foto Larissa Matos' className='img-page-sobre' />
-      <section>
-        <h2>kjgbhj</h2>
-        <p>jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</p>
-      </section> */}
+      <img src={Me} alt='Foto Larissa Matos' className='img-page-sobre' />
+      <span className='text-sobre'>
+        <h2 className='title-text-sobre'>Sobre mim</h2>
+        <h4 className='title-text-sobre'>Quem eu sou por trás da lente?!</h4>
+        <p>
+          Sou Larissa Matos, fotógrafa, atualmente trabalhando com ensaios intímos e de casais. <br />
+          Moro na Vila Andrade, Zona Sul do município de São Paulo. <br/> 
+          <br/>
+          Em 2022, iniciei meus trabalhos voltados à nú artístico. A idéia é mostrar às pessoas
+          a arte que habita em cada corpo e como ela se torna única, com novos traços, cores, curvas...
+          Trazendo novas histórias, momentos, sorrisos, olhares, que ficarão para sempre na memória de cada um!
+        </p>
+      </span>
     </div>
   )
 }
